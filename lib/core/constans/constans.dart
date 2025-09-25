f7fa57114dd1f55684172e4f31ab071e0c5214c8
Shahdad <skylla3784@gmail.com>
2025-09-25 11:43:16 +0330

    add homePage data Constans

--- lib/core/constans/constans.dart
+++ lib/core/constans/constans.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/features/home/data/models/homepage_data.dart';
 
 class AppConstans {
   static const String hotelsData = "assets/data/hotels.json";
@@ -25,4 +26,22 @@ class AppColors {
   static const Color darkBorder = Color(0xff707070);
   static const Color darkFocusBorder = Colors.brown;
   static const Color darkButton = Color(0xff5d4037);
+}
+
+class HomePageDataConstants {
+  static const List<String> _favoriteHotelIds = ["1", "3", "5", "7"];
+  static const List<String> _discountedHotelIds = ["2", "4", "6", "8"];
+  static const List<String> _recentlyViewedHotelIds = ["1", "4", "9"];
+  static const List<String> _popularHotelIds = ["3", "6", "9", "10"];
+  static const List<String> _specialOfferHotelIds = ["5", "7", "10"];
+  static const List<String> _newestHotelIds = ["8", "9", "10"];
+
+  static HomepageData get homePageData => HomepageData(
+    favorires: _favoriteHotelIds,
+    discounted: _discountedHotelIds,
+    recentlyViewed: _recentlyViewedHotelIds,
+    popular: _popularHotelIds,
+    specialOffers: _specialOfferHotelIds,
+    newest: _newestHotelIds,
+  );
 }
---
+++ lib/features/home/data/models/homepage_data.dart
@@ -0,0 +1,17 @@
+class HomepageData {
+  final List<String> favorires;
+  final List<String> discounted;
+  final List<String> recentlyViewed;
+  final List<String> popular;
+  final List<String> specialOffers;
+  final List<String> newest;
+
+  HomepageData({
+    required this.favorires,
+    required this.discounted,
+    required this.recentlyViewed,
+    required this.popular,
+    required this.specialOffers,
+    required this.newest,
+  });
+}
