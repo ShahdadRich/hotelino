11daf94af09c4e8c22cf6e9b7d80ed3c01da35fc
Shahdad <skylla3784@gmail.com>
2025-11-03 21:28:51 +0330

    add setter and getter for booking data (booking provider)

--- lib/features/booking/presentation/booking_provider.dart
+++ lib/features/booking/presentation/booking_provider.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/widgets.dart';
+import 'package:hotelino/features/booking/data/booking_data.dart';
+import 'package:persian_datetime_picker/persian_datetime_picker.dart';
 
+class BookingProvider with ChangeNotifier {
+  final BookingData _booking = BookingData();
+  BookingData get booking => _booking;
+
+  void setFullName(String value) {
+    _booking.fullName = value;
+    notifyListeners();
+  }
+
+  void setDestination(String value) {
+    _booking.destination = value;
+    notifyListeners();
+  }
+
+  void setNumberOfGuests(String value) {
+    _booking.numberOfGuests = value;
+    notifyListeners();
+  }
+
+  void setPhoneNumber(String value) {
+    _booking.phoneNumber = value;
+    notifyListeners();
+  }
+
+  void setRangeDate(JalaliRange value) {
+    _booking.checkInOutRageDate = value;
+    notifyListeners();
+  }
+}
